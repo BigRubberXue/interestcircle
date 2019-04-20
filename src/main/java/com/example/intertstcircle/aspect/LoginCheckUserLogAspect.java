733d25728cd2754807ba042c3df6cdb5b8546290
e <617044299@qq.com>
2019-04-20 22:44:08 +0800

    aspect

---
+++ src/main/java/com/example/intertstcircle/aspect/LoginCheckUserLogAspect.java
@@ -0,0 +1,31 @@
+package com.example.intertstcircle.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoginCheckUserLogAspect {
+
+
+    @Pointcut("execution(public * com.example.intertstcircle.Service.UserSignService.checkUserExist(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 接收到请求，记录请求内容
+        log.info("正在查询数据" );
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        log.info("查询数据结束   ");
+    }
+}
---
+++ src/main/java/com/example/intertstcircle/aspect/LoginInsertLogAspect.java
@@ -0,0 +1,34 @@
+package com.example.intertstcircle.aspect;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class LoginInsertLogAspect {
+
+
+    @Pointcut("execution(public * com.example.intertstcircle.Service.UserSignService.insertUser(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 接收到请求，记录请求内容
+        log.info("正在插入数据" );
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        log.info("插入数据结束   ");
+    }
+
+}
